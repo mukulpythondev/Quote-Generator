73bbb02d0fc9f28b937b12df6fc27112086e67be
Mukul Rana <134795697+mukulpythondev@users.noreply.github.com>
2024-04-01 11:33:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
-import Card from './Components/Card'
+import Card from './Components/Cards'
 
 
 function App() {
   return (
     <div className='h-screen w-screen bg-gradient-to-br from-indigo-800 to-purple-400 overflow-hidden' >
       <Navbar/>
-      <Card/>
+      <Cards/>
     </div>
   )
 }
